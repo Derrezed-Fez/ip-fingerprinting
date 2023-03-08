1be33d7110d2514f4eb33b282dc99b779dd4fe39
Zane Pelletier <zpelleti@umich.edu>
2023-03-08 13:13:15 -0500

    Adding sterr handling for process execution and support for Safari.

--- browsertime_crawler.py
+++ browsertime_crawler.py
@@ -2,24 +2,31 @@ import subprocess, csv, datetime, os
 logfile_name = 'data\\output\\' + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '_logfile.log'
 logfile = open(logfile_name, 'w+')
 def crawl_firefox(url):
-    p = subprocess.Popen(["powershell.exe", "C:\\Users\\Zane\\ip-fingerprinting\\crawl_firefox.ps1", 'https://www.' + url], stdout=logfile)
+    p = subprocess.Popen(["powershell.exe", "C:\\Users\\Zane\\ip-fingerprinting\\crawl_firefox.ps1", 'https://www.' + url], stdout=logfile, stderr=logfile)
     p.communicate()
 
 def crawl_chrome(url):
-    p = subprocess.Popen(["powershell.exe", "C:\\Users\\Zane\\ip-fingerprinting\\crawl_chrome.ps1", 'https://www.' + url], stdout=logfile)
+    p = subprocess.Popen(["powershell.exe", "C:\\Users\\Zane\\ip-fingerprinting\\crawl_chrome.ps1", 'https://www.' + url], stdout=logfile, stderr=logfile)
     p.communicate()
 
 def crawl_edge(url):
-    p = subprocess.Popen(["powershell.exe", "C:\\Users\\Zane\\ip-fingerprinting\\crawl_edge.ps1", 'https://www.' + url], stdout=logfile)
+    p = subprocess.Popen(["powershell.exe", "C:\\Users\\Zane\\ip-fingerprinting\\crawl_edge.ps1", 'https://www.' + url], stdout=logfile, stderr=logfile)
     p.communicate()
 
-def crawl_domains(domains:str):
+def crawl_safari(url):
+    p = subprocess.Popen(["browsertime", 'https://www.' + url, '-b', 'safari'], stdout=logfile, stderr=logfile)
+    p.communicate()
+
+def crawl_domains(domains:str, is_macos:bool=False):
     with open(domains, newline='\n') as f:
         reader = csv.reader(f)
         for row in reader:
-            crawl_chrome(row[0])
-            crawl_firefox(row[0])
-            crawl_edge(row[0])
+            if is_macos:
+                crawl_safari
+            else:
+                crawl_chrome(row[0])
+                crawl_firefox(row[0])
+                crawl_edge(row[0])
             # procs = [multiprocessing.Process(target=crawl_firefox, args=(row[0], )), \
             #          multiprocessing.Process(target=crawl_chrome, args=(row[0], )), \
             #         multiprocessing.Process(target=crawl_edge, args=(row[0], ))]
