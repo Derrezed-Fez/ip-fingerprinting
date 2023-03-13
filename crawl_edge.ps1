0f3e5659bd31483809ec23c8b7daa6282d3cb87b
Zane Pelletier <zpelleti@umich.edu>
2023-03-13 17:29:53 -0400

    Modifying PS scripts to include output folder path and additional extras (for different systems).

--- crawl_edge.ps1
+++ crawl_edge.ps1
@@ -1,3 +1,5 @@
 $url=$args[0]
+$domain=$args[1]
+$time=$args[2]
 Set-Location "C:\Users\psych\AppData\Roaming\npm"
-browsertime $url -b edge --headless --output edge_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000 --resultDir "F:\\crawler"
+browsertime $url -b edge --headless --output edge_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000 --resultDir "F:\crawler\$($domain)\$($time)" --edge.edgedriverPath "C:\msedgedriver.exe"
