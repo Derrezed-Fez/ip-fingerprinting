3c25522c1b0b67e7fdfad52129cd41480b32aaa4
ashyaka <42945150+ashyaka@users.noreply.github.com>
2023-03-19 00:05:14 -0400

    Update crawl_brave.ps1

--- crawl_brave.ps1
+++ crawl_brave.ps1
@@ -1,3 +1,9 @@
-$url=$args[0]
-Set-Location "C:\Users\Zane\AppData\Roaming\npm"
-browsertime $url --chrome.binaryPath "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe" --headless --output edge_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000
+# crawl_brave.ps1
+param (
+    [string]$url,
+    [string]$domain,
+    [string]$time
+)
+
+Set-Location "C:\Users\shyak\AppData\Roaming\npm"
+browsertime $url -b chrome --chrome.binaryPath "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --headless --output brave_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000 --resultDir "C:\Fingerprinting\$($domain)\$($time)"
