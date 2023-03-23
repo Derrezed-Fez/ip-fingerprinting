a575c83bbe249b87e31d3da80b16e5620c8165ee
jjack151 <jonnijackson17@gmail.com>
2023-03-23 14:54:15 -0400

    corrected mistake regarding binaryPath

--- crawl_brave.ps1
+++ crawl_brave.ps1
@@ -6,5 +6,5 @@ param (
 )
 
 Set-Location "C:\Users\17862\AppData\Roaming\npm"
-browsertime $url -b brave --brave.binaryPath "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --headless --output brave_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000 --resultDir "C:\Fingerprinting\$($domain)\Brave\$($time)"
+browsertime $url -b brave --chrome.binaryPath "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --headless --output brave_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000 --resultDir "C:\Fingerprinting\$($domain)\Brave\$($time)"
 
