1da0c3a8c7c27a4bd9c84aab05b0d5c5e2ddddca
jjack151 <jonnijackson17@gmail.com>
2023-03-23 14:29:39 -0400

    Removed Footer

--- crawl_brave.ps1
+++ crawl_brave.ps1
@@ -7,4 +7,4 @@ param (
 
 Set-Location "C:\Users\17862\AppData\Roaming\npm"
 browsertime $url -b brave --brave.binaryPath "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --headless --output brave_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000 --resultDir "C:\Fingerprinting\$($domain)\Brave\$($time)"
-Footer
+
