866351024a605a789b109f5d4ba08cd26e3c5afc
jjack151 <jonnijackson17@gmail.com>
2023-03-23 13:13:12 -0400

    Changes to crawl through all 4 browsers

--- crawl_chrome.ps1
+++ crawl_chrome.ps1
@@ -1,5 +1,5 @@
 $url=$args[0]
 $domain=$args[1]
 $time=$args[2]
-Set-Location "C:\Users\psych\AppData\Roaming\npm"
-browsertime $url -b chrome --headless --output chrome_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000 --resultDir "F:\crawler\$($domain)\$($time)"
+Set-Location "C:\Users\17862\AppData\Roaming\npm"
+browsertime $url -b chrome --headless --output chrome_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000 --resultDir "C:\Fingerprinting\$($domain)\Chrome\$($time)"
--- crawl_edge.ps1
+++ crawl_edge.ps1
@@ -1,5 +1,5 @@
 $url=$args[0]
 $domain=$args[1]
 $time=$args[2]
-Set-Location "C:\Users\psych\AppData\Roaming\npm"
-browsertime $url -b edge --headless --output edge_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000 --resultDir "F:\crawler\$($domain)\$($time)" --edge.edgedriverPath "C:\msedgedriver.exe"
+Set-Location "C:\Users\17862\AppData\Roaming\npm"
+browsertime $url -b edge --edge.binaryPath "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --headless --output edge_results --timeouts.browserStart 15000 --timeouts.pageCompleteCheck 15000 --resultDir "C:\Fingerprinting\$($domain)\Edge\$($time)" 
