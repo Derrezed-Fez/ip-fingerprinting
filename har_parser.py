2f89eda0d2fd77fd8a502a55111a69083b3e19aa
Zane Pelletier <zpelleti@umich.edu>
2023-03-24 19:18:06 -0400

    Refactoring the parser.

--- har_parser.py
+++ har_parser.py
@@ -1,14 +1,18 @@
 import json, os
 from haralyzer import HarParser
 
-LOG_FILEPATH = 'D:\\IP Domain Fingerprinting\\New Data Collection'
+LOG_FILEPATH = 'C:\\Users\\hacki\\ip-fingerprinting\\data\\input\\New Data Collection'
 
 def convert_ip_to_int(ip:str):
     octets = ip.split('.')
     return (int(octets[0]) * (256**3)) + (int(octets[1]) * (256**2)) + (int(octets[2]) * 256) + int(octets[3])
 
-def calculate_basic_domain_based_fingerprint(dir:str, domain:str):
+def compile_fingerprints_and_traces(dir:str, domain:str):
+    connections = list()
     browser = ''
+    network_trace_filepath = os.path.join(LOG_FILEPATH, 'Network Traces')
+    if not os.path.isdir(network_trace_filepath):
+        os.mkdir(network_trace_filepath)
     domain_placed = False
     fingerprint = domain.replace('www.', '') + ';'
     secondary_reqs = list()
@@ -20,12 +24,16 @@ def calculate_basic_domain_based_fingerprint(dir:str, domain:str):
                 browser = 'chrome'
             elif 'firefox' in file:
                 browser = 'firefox'
+            elif 'brave' in file:
+                browser = 'brave'
+            elif 'safari' in file:
+                browser = 'safari'
         if '.har' in file:
             with open(os.path.join(dir, file), 'r', encoding='utf-8') as f:
                 har_parser = HarParser(json.loads(f.read()))
                 for page in har_parser.pages:
                     for entry in page.entries:
-                        if har_parser.match_request_type(entry, 'GET'):
+                        if har_parser.match_request_type(entry, 'GET') or har_parser.match_request_type(entry, 'POST'):
                             try:
                                 if entry.url == 'https://' + domain + '/' and not domain_placed:
                                     fingerprint += (str([convert_ip_to_int(entry.serverAddress)]) + ';')
@@ -35,55 +43,27 @@ def calculate_basic_domain_based_fingerprint(dir:str, domain:str):
                                         secondary_reqs.append(convert_ip_to_int(entry.serverAddress))
                             except:
                                 pass
-                        if har_parser.match_request_type(entry, 'POST'):
-                            try:
-                                if convert_ip_to_int(entry.serverAddress) not in secondary_reqs:
-                                    secondary_reqs.append(convert_ip_to_int(entry.serverAddress))
-                            except:
-                                pass
+                        try:
+                            if (har_parser.match_request_type(entry, 'GET') or har_parser.match_request_type(entry, 'POST')) and len(entry.timings.keys()) > 0:
+                                connections.append((entry.timings['receive'], convert_ip_to_int(entry.serverAddress)))
+                        except:
+                            pass
+                        # INSERT DOMAIN-Based Fingerprints HERE
+    with open(os.path.join(network_trace_filepath, browser + '_' + domain + '.txt'), 'w') as f:
+        f.write(str(connections) + '\n')
     if len(secondary_reqs) > 0:
         fingerprint += str(secondary_reqs)
     else:
         fingerprint = None
     return browser, fingerprint
 
-def network_trace_domain(dir, domain):
-    browser = ''
-    connections = list()
-    network_trace_filepath = os.path.join(LOG_FILEPATH, 'Network Traces')
-    if not os.path.isdir(network_trace_filepath):
-        os.mkdir(network_trace_filepath)
-    
-    for file in os.listdir(dir):
-        if '.json' in file:
-            if 'edge' in file:
-                browser = 'edge'
-            elif 'chrome' in file:
-                browser = 'chrome'
-            elif 'firefox' in file:
-                browser = 'firefox'
-        if '.har' in file:
-            with open(os.path.join(dir, file), 'r', encoding='utf-8') as f:
-                har_parser = HarParser(json.loads(f.read()))
-                for page in har_parser.pages:
-                    for entry in page.entries:
-                        try:
-                            if har_parser.match_request_type(entry, 'GET') and len(entry.timings.keys()) > 0:
-                                connections.append((entry.timings['receive'], convert_ip_to_int(entry.serverAddress)))
-                            if har_parser.match_request_type(entry, 'POST') and len(entry.timings.keys()) > 0:
-                                connections.append((entry.timings['receive'], convert_ip_to_int(entry.serverAddress)))
-                        except:
-                            pass
-    with open(os.path.join(network_trace_filepath, browser + '_' + domain + '.txt'), 'w') as f:
-        f.write(str(connections) + '\n')
-
-def generate_basic_ip_fingerprints():
+if __name__ == '__main__':
     chrome_basic_ip_fingerprints, firefox_basic_ip_fingerprints, edge_basic_ip_fingerprints = open(os.path.join(LOG_FILEPATH, 'chrome_basic_ip_fingerprints'), 'w'), \
         open(os.path.join(LOG_FILEPATH, 'firefox_basic_ip_fingerprints'), 'w'), open(os.path.join(LOG_FILEPATH, 'edge_basic_ip_fingerprints'), 'w')
     for dir in os.listdir(LOG_FILEPATH):
-        if '.log' not in dir and 'fingerprints' not in dir:
+        if '.log' not in dir and 'fingerprints' not in dir and 'Network' not in dir:
             for inner_dir in os.listdir(os.path.join(LOG_FILEPATH, dir)):
-                browser, fingerprint = calculate_basic_domain_based_fingerprint(os.path.join(LOG_FILEPATH, dir, inner_dir), dir)
+                browser, fingerprint = compile_fingerprints_and_traces(os.path.join(LOG_FILEPATH, dir, inner_dir), dir)
                 if fingerprint is not None:
                     if 'chrome' in browser:
                         chrome_basic_ip_fingerprints.write(fingerprint + '\n')
@@ -94,12 +74,3 @@ def generate_basic_ip_fingerprints():
     chrome_basic_ip_fingerprints.close()
     firefox_basic_ip_fingerprints.close()
     edge_basic_ip_fingerprints.close()
-
-def generate_network_traces():
-    for dir in os.listdir(LOG_FILEPATH):
-        if '.log' not in dir and 'fingerprints' not in dir and 'Network Traces' not in dir:
-            for inner_dir in os.listdir(os.path.join(LOG_FILEPATH, dir)):
-                network_trace_domain(os.path.join(LOG_FILEPATH, dir, inner_dir), dir)
-
-if __name__ == '__main__':
-    generate_network_traces()
